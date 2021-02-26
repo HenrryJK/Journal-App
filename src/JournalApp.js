793ad5a7c4e34279bd1ca23857d338f980be4de2
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-26 14:12:53 -0500

    Componente cuando no hay nada seleccionado #8

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import { Sidebar } from './components/journal/Sidebar'
-
+import { AppRouter } from './routers/AppRouter'
 
 export const JournalApp = () => {
     return (
-        <div className="journal__main-content">
-            <Sidebar/>
-        <main>
-            <h1> Main content</h1>
-
-        </main>
-        
-        </div>
-
+       <AppRouter/>
     )
 }
--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -25,7 +25,5 @@ export const Sidebar = () => {
 
             <JournalEntries/>
            </aside>
-
-   
     )
 }
