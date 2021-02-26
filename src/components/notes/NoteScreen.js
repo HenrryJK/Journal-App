4ec063b7c3985b164edd0eb4455494c396accc9e
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-26 14:48:17 -0500

    Estructura y diseño del HomeScreen #10

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-conter">
             <NotesAppBar/>
+        <div className="notes__content">
+        <input
+        type="text"
+        placeholder="Some awsome a tittle"
+        className="notes_tittle-input"
+        autoComplete="off"
+        />
+        <textarea
+        placeholder="What are you doing today ?"
+        className="notes_textarea" 
+        ></textarea>
+        <div className="notes_image">
+        <img
+        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+        alt="imagen"
+       />
         </div>
+
+        </div>
+        </div>
+        
     )
 }
