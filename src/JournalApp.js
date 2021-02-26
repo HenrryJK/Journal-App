59cf6f96d43ca37a796d486dcedb00d83ba3cc1c
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-26 13:25:53 -0500

    Aplicando algunos cambios al Sidebar (scss) #6

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Sidebar } from './components/journal/Sidebar'
 import { AppRouter } from './routers/AppRouter'
 
 export const JournalApp = () => {
     return (
-       <AppRouter/>
+        <div className="journal__main-content">
+            <Sidebar/>
+        <main>
+            <h1> Main content</h1>
+
+        </main>
+        
+        </div>
+
     )
 }
