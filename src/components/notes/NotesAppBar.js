7afa5ba09a3b2835de6621d757fdaadb8e4caae1
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-26 14:25:34 -0500

    Creando el NoteAppBar componete #9

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes__appbar">
+            <span>13 de julio de 1976</span>
+        <div>
+            <button className="btn">
+            Picture
+            </button>
+            <button className="btn">
+            Save
+            </button>
+        </div>
+        
+        </div>
+    )
+}
